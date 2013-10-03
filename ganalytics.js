98d41513620d4c52761796ed39911cad029a4859
Henrotte Julien <hoshi.graphi@gmail.com>
2013-10-03 13:37:50 +0200

    Plugin + New release for G Analytics (still old version for fallback)

--- ganalytics.js
+++ ganalytics.js
@@ -1,21 +1,42 @@
-var setAccout = 'UA-28339293-1';
+$.fn.janalytics = function( options ) {
+  var settings = $.extend({
+          setAccount: "",
+          tag: "body",
+          selector : ".trueLink",
+          newRelease : true
+        }, options );
 
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', setAccount]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-
-  $('li, a').on('click', '.trueLink', function(event) {
-  	analytics($(this));
-  });
-
-
-  function analytics(element){
-  	_gaq.push(['_trackEvent', element.attr('data-category'), element.attr('data-action'), element.attr('data-label')]);
+  function analytics(category, action, label){
+    if(!settings.newRelease){
+      _gaq.push(['_trackEvent', category, action, label]);
+    }else{
+      ga('send', 'event', category, action, label);
+    }
   }
+
+  //if you have the older code like this set false for the newRelease
+  if (!settings.newRelease) {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', settings.setAccount]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  }else{
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', settings.setAccount, window.location.hostname);
+    ga('send', 'pageview');
+  }
+
+  $(settings.tag).on('click', settings.selector , function(event) {
+    event.preventDefault();
+    analytics(this.getAttribute("data-category"),this.getAttribute("data-action"), this.getAttribute('data-label'));
+  });
+};
