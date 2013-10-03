3103403df4cd510e0e4dc7e0443d8f5d6b26b940
Henrotte Julien <hoshi.graphi@gmail.com>
2013-10-03 23:54:52 +0200

    Bug fix + ©

---
+++ js/ganalytics.js
@@ -0,0 +1,58 @@
+/* ===========================================================
+ * Janalytics.js v0.1 - Beta
+ * ===========================================================
+ * Copyright 2013 Julien Henrotte.
+ * http://www.da-ha.be
+ *
+ * Little jQuery plugin to create custom event for Google Analytics.
+ *
+ * Credit: Eike Send for the awesome swipe event
+ * https://github.com/peachananr/onepage-scroll
+ *
+ * ========================================================== */
+
+$.fn.janalytics = function( options ) {
+  var settings = $.extend({
+          setAccount: "",
+          tag: "body",
+          selector : ".trueLink",
+          newRelease : true,
+          prevent : false
+        }, options );
+
+  function analytics(category, action, label){
+    if(!settings.newRelease){
+      _gaq.push(['_trackEvent', category, action, label]);
+    }else{
+      ga('send', 'event', category, action, label);
+    }
+  }
+
+  //if you have the older code like this set false for the newRelease
+  if (!settings.newRelease) {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', settings.setAccount]);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  }else{
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', settings.setAccount, window.location.hostname);
+    ga('send', 'pageview');
+  }
+
+  $(settings.tag).on('click', settings.selector , function(event) {
+    if(settings.prevent){
+      event.preventDefault();
+    }
+    analytics(this.getAttribute("data-category"),this.getAttribute("data-action"), this.getAttribute('data-label'));
+  });
+};
